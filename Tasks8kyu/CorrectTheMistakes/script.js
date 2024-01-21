2f2ae566ca2d3882d49fc634a4577e975a3677cb
Arefevpro <arefevmsk@gmail.com>
2024-01-21 18:21:11 +0300

    script.js

--- Tasks8kyu/CorrectTheMistakes/script.js
+++ Tasks8kyu/CorrectTheMistakes/script.js
@@ -5,7 +5,7 @@
 // When documents (especially pretty old ones written with a typewriter), 
 // are digitised character recognition softwares often make mistakes.
 // Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
-
+// Example
 // S is misinterpreted as 5
 // O is misinterpreted as 0
 // I is misinterpreted as 1
@@ -13,7 +13,7 @@
 
 
 
-// Example
+// Decision:
 
 function correct(string)
 {
