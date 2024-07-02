e3af445807205fc851a3f95e8d9a76ea172f0aa4
Arefevpro <arefevmsk@gmail.com>
2024-07-02 18:40:22 +0300

    ./

---
+++ Tasks8kyu/ReversedStrings.js
@@ -0,0 +1,7 @@
+//ОПИСАНИЕ:
+// Завершите решение так, чтобы оно перевернуло переданную в него строку.
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+function solution(str){
+  return str.split('').reverse().join('')
+  }
