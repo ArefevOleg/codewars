d113311cd7b38930860e2f786bc51c49fef2a6d3
Arefevpro <arefevmsk@gmail.com>
2024-07-04 20:14:48 +0300

    ./

--- Tasks8kyu/MathematicalOperations.js
+++ Tasks8kyu/MathematicalOperations.js
@@ -10,3 +10,16 @@
 function basicOp(operation, value1, value2){
   return eval(value1 + operation + value2)
 }
+
+// if( operation == "+") {
+//   return value1 + value2
+// }
+// if( operation == "-") {
+//   return value1 - value2
+// }
+// if( operation == "*") {
+//   return value1 * value2
+// }
+// if( operation == "/") {
+//   return value1 / value2
+// }
