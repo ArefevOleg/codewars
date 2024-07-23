779f2754bba9f905d7d78017fa8a094772acd014
Arefevpro <arefevmsk@gmail.com>
2024-07-23 23:01:17 +0300

    ./

--- Tasks7kyu/NumberPairs.js
+++ Tasks7kyu/NumberPairs.js
@@ -7,16 +7,16 @@
 // let arr2 = [23, 14, 53, 17, 80];
 // getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88]
 
-function getLargerNumbers(a, b) {
-  const result = [];
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] > b[i]) {
-      result.push(a[i]);
-    } else {
-      result.push(b[i]);
-    }
-  }
-  return result
-}
-
-console.log(getLargerNumbers([13, 64, 15, 17, 88],[23, 14, 53, 17, 80]))
+// function getLargerNumbers(a, b) {
+//   const result = [];
+//   for (let i = 0; i < a.length; i++) {
+//     if (a[i] > b[i]) {
+//       result.push(a[i]);
+//     } else {
+//       result.push(b[i]);
+//     }
+//   }
+//   return result
+// }
+//
+// console.log(getLargerNumbers([13, 64, 15, 17, 88],[23, 14, 53, 17, 80]))
