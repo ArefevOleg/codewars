d8b7793c7b5f87635f3b0cd7bb963405479db9e9
Arefevpro <arefevmsk@gmail.com>
2024-09-17 15:32:27 +0300

    ./

--- Tasks8kyu/SortedIntegerArrays.js
+++ Tasks8kyu/SortedIntegerArrays.js
@@ -4,5 +4,9 @@
 // конечный результат должен быть отсортирован и не иметь дубликатов.
 
 function mergeArrays(a, b) {
-  return
+  const newArr = [...a, b].flat(Infinity).sort((a,b) => a - b)
+  return Array.from(new Set(newArr))
+  // return [...new Set(a.concat(b))].sort((a,b)=>a-b);
 }
+
+console.log(mergeArrays([2, 4, 8], [2, 4, 6]))
